b48a0de44cc7f2b10568ecb49db95d494dab5627
JohnThunder01 <109166944+JohnThunder01@users.noreply.github.com>
2022-07-12 20:25:47 +0300

    GuessTheNumber.py
    This is a simple guess the number game on the python terminal

---
+++ GuessTheNumber.py
@@ -0,0 +1,16 @@
+import random
+
+def guess(x):
+    random_number = random.randint (1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}:'))
+        if guess < random_number:
+            print('Sorry, guess again. Tool low.')
+        elif guess > random_number:
+            print('Sorry,guess again. Too high.')
+
+    print(f'Congrats, You guessed the correct number {random_number}')
+
+
+guess(10)
